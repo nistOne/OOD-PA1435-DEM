24c60b94d0fa01d4343694232294456de4e6789c
MARCUS HOLMBERG <maip15@student.bth.se>
2017-05-08 21:01:38 +0200

    Answer to David!

--- DEM/DEM/Main.cpp
+++ DEM/DEM/Main.cpp
@@ -4,6 +4,7 @@
 int main()
 {
 	std::cout << "Yolo i bajs i frillen" << std::endl;
+	std::cout << "Nemen Daaavid daaaa! ;)" << std::endl;
 
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
