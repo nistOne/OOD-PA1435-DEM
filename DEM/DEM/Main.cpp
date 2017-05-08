84e70059921ec12e7b7b217b9834d75c62485dc0
MARCUS HOLMBERG <maip15@student.bth.se>
2017-05-08 18:45:29 +0200

    Answer to Emma

--- DEM/DEM/Main.cpp
+++ DEM/DEM/Main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	std::cout << "OMG!!! That EMMA KRONHOLM is to damn cute!!! I'm gonna die!!!" << std::endl;
+	std::cout << "HEY EMMA!! :D NICE TO MEED YOU!" << std::endl;
 
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
