a82f219e96cbb7887aa5e73d9b37a06b3faef0af
MARCUS HOLMBERG <maip15@student.bth.se>
2017-05-08 17:50:04 +0200

    SMFL is working

--- DEM/DEM/Main.cpp
+++ DEM/DEM/Main.cpp
@@ -1,9 +1,27 @@
+#include "SFML-2.4.2/include/SFML/Graphics.hpp"
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	std::cout << "OMG!!! That Lars Marcus Holmberg is to damn cute!!! I'm gonna die!!!" << std::endl;
 
-	getchar();
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
