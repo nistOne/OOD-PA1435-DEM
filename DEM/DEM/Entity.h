8f763ffd9d00b7ec14e6a05edf96bf2c305d3fc8
Discojanne <db96@live.se>
2017-05-16 21:00:11 +0200

    retry

--- DEM/DEM/Entity.h
+++ DEM/DEM/Entity.h
@@ -13,11 +13,13 @@ protected:
 	std::string name;
 	sf::Sprite sprite;
 	sf::Vector2i pos;
+	sf::Vector2i targetPos;
 public:
 	Entity();
 	Entity(int x, int y, int hitPoint, int strenght, std::string name);
 	virtual ~Entity();
 	sf::Vector2i getPos()const;
+	sf::Vector2i getTargetPos()const;
 	float getStrength()const;
 	bool update();
 	void takeDamage(int damage);
